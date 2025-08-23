353007ff2228c16219d6e2ad2d19d38e7c530115
ad-altun <adaltun@web.de>
2025-08-23 20:21:53 +0200

    frontend - add Home component and a table that will be list the products, add table head as well

---
+++ frontend/src/components/Home.tsx
@@ -0,0 +1,27 @@
+
+export default function Home() {
+
+    return (
+        <div className="home-container">
+
+            {/*    Table area*/}
+            <div className="table-container">
+                <table>
+                    <thead className='table-head'>
+                    <tr>
+                        <th>No</th>
+                        <th>Product</th>
+                        <th>SKU</th>
+                        <th>Location</th>
+                        <th>Price</th>
+                        <th>Stock</th>
+                        <th>Action</th>
+                    </tr>
+                    </thead>
+
+                </table>
+            </div>
+
+        </div>
+    );
+};
