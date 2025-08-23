cdce3dcba8824d86c3119c2721641b640e0044bb
ad-altun <adaltun@web.de>
2025-08-23 20:46:39 +0200

    frontend - add search input element (just ui element, no functionality) for product search

--- frontend/src/components/HeaderControl.tsx
+++ frontend/src/components/HeaderControl.tsx
@@ -3,7 +3,12 @@ export default function HeaderControl() {
  return (
   <div>
       <div className="main-content">
-
+          <div className='header-controls'>
+              {/*Header area*/}
+              <div className="search-box">
+                  <input type='text' placeholder='Search...'/>
+              </div>
+          </div>
       </div>
   </div>
  );
