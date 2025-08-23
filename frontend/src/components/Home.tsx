5432cd30310b271213f5bcea9efee7fe9c7a6130
ad-altun <adaltun@web.de>
2025-08-23 23:49:13 +0200

    frontend - add product table data and props

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,5 +1,13 @@
+import type {Product} from "../types/types.ts";
 
-export default function Home() {
+export type ProductTableProps = {
+    products: Product[]
+    onProductEditButtonClicked: (product: Product) => void
+    onProductDetailsButtonClicked: (product: Product) => void
+    onProductDeleteButtonClicked: (product: Product) => void
+}
+
+export default function Home(props: Readonly<ProductTableProps>) {
 
     return (
         <div className="home-container">
@@ -18,7 +26,46 @@ export default function Home() {
                         <th>Action</th>
                     </tr>
                     </thead>
+                    <tbody className='table-body'>
+                        {
+                            props.products &&
+                            props.products.map((product, index) => {
+                                return (
+                                    <tr key={product.id}>
+                                        <td className="table-data">
+                                            {index+1}
+                                        </td>
+                                        <td className="table-data">
+                                            {product.name}
+                                        </td>
+                                        <td className="table-data">
+                                            {product.stockKeepingUnit}
+                                        </td>
+                                        <td className="table-data">
+                                            {product.location}
+                                        </td>
+                                        <td className="table-data">
+                                            {product.price}
+                                        </td>
+                                        <td className="table-data">
+                                            {product.quantity}
+                                        </td>
+                                        <td className=''>
+                                            <button className={"productButton"}
+                                                    onClick={() => props.onProductDetailsButtonClicked(product)}>Details</button>
+                                            <button className={"productButton"}
+                                                    onClick={() => props.onProductEditButtonClicked(product)}>Edit</button>
+                                            <button className={"miniButton"}
+                                                    style={{backgroundColor:"#b00",color:"white",fontWeight:"bold"}}
+                                                    onClick={() => {
+                                                        props.onProductDeleteButtonClicked(product)
+                                                    }}>X</button></td>
+                                    </tr>
+                                )
+                            })
+                        }
 
+                    </tbody>
                 </table>
             </div>
 
