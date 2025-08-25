7c6750caa3acc64b92fe26465ec337ba62e6f50d
ad-altun <adaltun@web.de>
2025-08-25 15:27:51 +0200

    frontend - move main header to HeaderControls component

--- frontend/src/components/HeaderControl.tsx
+++ frontend/src/components/HeaderControl.tsx
@@ -2,20 +2,23 @@ type HeaderControlProps = {
     onAddProductClick: () => void;
 };
 
-export default function HeaderControl({ onAddProductClick }: HeaderControlProps) {
- return (
-  <div>
-          <div className='header-controls'>
-              {/*Header area*/}
-              <div className="search-box">
-                  <input type='text' placeholder='Search...'/>
-              </div>
-              <div className="table-toolbar">
-                  <button className="productButton" onClick={onAddProductClick}>
-                      Add Product
-                  </button>
-              </div>
-          </div>
-  </div>
- );
+export default function HeaderControl({onAddProductClick}: HeaderControlProps) {
+    return (
+        <div>
+            <div className='app-title'>
+                <h1>Warehouse Management</h1>
+            </div>
+            <div className='header-controls'>
+                {/*Header area*/}
+                <div className="search-box">
+                    <input type='text' placeholder='Search...'/>
+                </div>
+                <div className="table-toolbar">
+                    <button className="productButton" onClick={onAddProductClick}>
+                        Add Product
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
 };
