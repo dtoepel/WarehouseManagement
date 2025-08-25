99414fb69ba366595b9e5b05dd3a85b1891b7880
ad-altun <adaltun@web.de>
2025-08-26 00:45:27 +0200

    frontend - add handleChange and handleSubmit functions in EditProduct component

--- frontend/src/components/EditProduct.tsx
+++ frontend/src/components/EditProduct.tsx
@@ -1,5 +1,36 @@
+import type {Product} from "../types/types.ts";
+import {type FormEvent, useState} from "react";
+import {editProduct} from "../service/api.ts";
 
-export default function EditProduct() {
+type EditProductProps = {
+    onProductEdit: (product: Product) => void;
+    onCancel: () => void;
+    product: Product;
+}
+
+export default function EditProduct({ onProductEdit, product, onCancel }: EditProductProps) {
+    const [productValue, setProductValue] = useState<Product>(product)
+
+    const handleChange = (field: keyof Product, value:string) => {
+        setProductValue((prevState) => {
+            return {
+                ...prevState,
+                [field]: value
+            }
+        })
+    }
+
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        try {
+            await editProduct(productValue)
+                .then(onProductEdit)
+                .catch(e => console.log("Error while editing product ", e))
+        } catch (error) {
+            console.log("Error while editing product" , error);
+        }
+    }
 
     return (
   <div>
